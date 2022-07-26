32245407420384846e70e58975b428771e79cede
giriftzen <enes.seveng37@gmail.com>
2022-07-26 17:36:55 +0200

    :sparkles: [INIT] project

---
+++ src/main/java/com/example/ecodationtest/EcodationTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.ecodationtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcodationTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcodationTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ecodationtest/EcodationTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ecodationtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcodationTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
