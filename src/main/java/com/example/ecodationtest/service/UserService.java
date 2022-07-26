31a1f9c81be587608ac2dd549b684a0575d932bc
giriftzen <enes.seveng37@gmail.com>
2022-07-26 19:08:59 +0200

    :ambulance: [EDIT] UserService

--- src/main/java/com/example/ecodationtest/service/UserService.java
+++ src/main/java/com/example/ecodationtest/service/UserService.java
@@ -1,8 +1,13 @@
 package com.example.ecodationtest.service;
 
+import com.example.ecodationtest.dto.UserDto;
 import com.example.ecodationtest.entity.UserEntity;
 import com.example.ecodationtest.repository.UserRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,19 +18,35 @@ public class UserService {
 
     public UserRepository userRepository;
 
-    public UserService(UserRepository userRepository) {
+    public ModelMapper modelMapper;
+
+    public PasswordEncoder passwordEncoder;
+
+    public UserService(UserRepository userRepository, ModelMapper modelMapper, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.modelMapper = modelMapper;
+        this.passwordEncoder = passwordEncoder;
     }
 
-    //save user
-    public UserEntity saveUser(UserEntity userEntity) {
-       return userRepository.save(userEntity);
+
+
+    public UserDto EntityToDto(UserEntity userEntity) {
+        return modelMapper.map(userEntity,UserDto.class);
     }
 
-    //List all users
-    public List<UserEntity> listAllUsers() {
+    public UserEntity DtoToEntity(UserDto userDto) {
+        return modelMapper.map(userDto,UserEntity.class);
+    }
 
-        return userRepository.findAll();
+    //Create
+
+    @PostMapping("users/create")
+    public UserDto createUser(@RequestBody UserDto userDto) {
+        String encrytedMasked=passwordEncoder.encode(userDto.getPassword());
+        userDto.setPassword(encrytedMasked);
+        UserEntity entity=DtoToEntity(userDto);
+        userRepository.save(entity);
+        return userDto;
     }
 
 
