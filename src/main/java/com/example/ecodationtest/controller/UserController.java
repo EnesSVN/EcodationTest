74ad5a45bcfd147d7a0daa882c33ab982c3c4e8f
giriftzen <enes.seveng37@gmail.com>
2022-07-26 18:50:10 +0200

    :fire: [create] Controller

---
+++ src/main/java/com/example/ecodationtest/controller/UserController.java
@@ -0,0 +1,13 @@
+package com.example.ecodationtest.controller;
+
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin("*")
+@RestController
+public class UserController {
+
+
+
+}
